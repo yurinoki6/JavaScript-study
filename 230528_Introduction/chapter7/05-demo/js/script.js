e4df17fafde05879068c34108aaa59ad4d7406cd
yurinoki6 <kuroehaneko@gmail.com>
2023-06-10 19:08:41 +0900

    02_demoのローディングの記述も加える

--- 230528_Introduction/chapter7/05-demo/js/script.js
+++ 230528_Introduction/chapter7/05-demo/js/script.js
@@ -1,4 +1,58 @@
 /*
+ローディングから画面遷移
+================================================ */
+const loadingAreaGrey = document.querySelector('#loading');
+const loadingAreaGreen = document.querySelector('#loading-screen');
+const loadingTaxt = document.querySelector('#loading p');
+
+window.addEventListener('load',() => {
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(
+    {
+      opacity: [1,0],
+      visibility:'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill:'forwards',
+    }
+  );
+
+  // ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate (
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+  // ローディング中テキスト
+  loadingTaxt.animate (
+    [
+      {
+        opacity: 1,
+        offset: .8 //80%
+      },
+      {
+        opacity: 0,
+        offset: 1 //80%
+      }
+    ],
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
+
+/*
 画像ギャラリー
 ================================================ */
 const mainImage = document.querySelector('.gallery-image img');
