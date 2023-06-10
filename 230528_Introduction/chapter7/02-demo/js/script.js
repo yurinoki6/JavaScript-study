e941fcb76c4782a55a57664460a8c6ab8e6cf6d2
yurinoki6 <kuroehaneko@gmail.com>
2023-06-10 16:06:04 +0900

    定数とアニメーションの指定

--- 230528_Introduction/chapter7/02-demo/js/script.js
+++ 230528_Introduction/chapter7/02-demo/js/script.js
@@ -1,6 +1,9 @@
 /*
 ローディングから画面遷移
 ================================================ */
+const loadingAreaGrey = document.querySelector('#loading');
+
 window.addEventListener('load',() => {
-  // ローディングが終わった後の処理
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(動かす内容,動きの詳細);
 });
