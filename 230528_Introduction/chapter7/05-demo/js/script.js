1157ed90ff2d03b8e354f451a6d35612eba45bd2
yurinoki6 <kuroehaneko@gmail.com>
2023-06-10 19:06:55 +0900

    複数のHTML要素を取得する

--- 230528_Introduction/chapter7/05-demo/js/script.js
+++ 230528_Introduction/chapter7/05-demo/js/script.js
@@ -1,3 +1,7 @@
 /*
 画像ギャラリー
 ================================================ */
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+
+console.log(thumbImages);
