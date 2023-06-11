e6d77189afebd4a2ce34c3484bd5266800524cb9
yurinoki6 <kuroehaneko@gmail.com>
2023-06-11 19:15:26 +0900

    Intersection Observerの設定をする

--- 230528_Introduction/chapter7/Fashion/js/script.js
+++ 230528_Introduction/chapter7/Fashion/js/script.js
@@ -116,32 +116,15 @@ menuClose.addEventListener('click', () => {
 /*
 スクロールで要素を表示
 ================================================ */
-// 監視対象が範囲内に現れたら実行する動作
-const animateFade = (entries, obs) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.animate(
-        {
-          opacity: [0, 1],
-          filter: ['blur(.4rem)', 'blur(0)'], 
-          translate: ['0 4rem', 0],
-        },
-        {
-          duration: 2000,
-          easing: 'ease',
-          fill: 'forwards',
-        }
-      );
-      // 一度ふわっと表示されたら監視をやめる
-      obs.unobserve(entry.target);
-    }
-  });
+//監視対象が範囲内に現れたら実行する動作
+const animateFade = () => {
+  console.log('ふわっ');
 };
 
 // 監視設定
 const fadeObserver = new IntersectionObserver(animateFade);
 
-// .fadeinを監視するよう指示
+// .fadenを監視するよう指示
 const fadeElements = document.querySelectorAll('.fadein');
 fadeElements.forEach((fadeElement) => {
   fadeObserver.observe(fadeElement);
