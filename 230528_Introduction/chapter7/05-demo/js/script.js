2b77712844bdc970c3221f406b228fb21fb2e628
yurinoki6 <kuroehaneko@gmail.com>
2023-06-11 17:06:10 +0900

    画像ギャラリーをカーソルを合わせた時に大きくなるように

--- 230528_Introduction/chapter7/05-demo/js/script.js
+++ 230528_Introduction/chapter7/05-demo/js/script.js
@@ -59,7 +59,8 @@ const mainImage = document.querySelector('.gallery-image img');
 const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
 
 for(let i = 0; i < thumbImages.length; i++) {
-  thumbImages[i].addEventListener('mouseover', () => {
+  thumbImages[i].addEventListener('mouseover', (event) => {
+    mainImage.src = event.target.src;
     mainImage.animate({opacity: [0, 1]}, 500);
   });
 }
