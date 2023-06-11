54c9759837955305cfdfd751c13df293db351da4
yurinoki6 <kuroehaneko@gmail.com>
2023-06-11 18:12:22 +0900

    メニューパネルをスライドさせるアニメーションを作る

--- 230528_Introduction/chapter7/09-demo/js/script.js
+++ 230528_Introduction/chapter7/09-demo/js/script.js
@@ -70,10 +70,21 @@ thumbImages.forEach((thumbImage)=>{
 スライドメニュー
 ================================================ */
 const menuOpen = document.querySelector('#menu-open');
-const menuClose = document.querySelector('#menuClose');
-const menuPanel = document.querySelector('#menu-Panel');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
 
 // メニューを開く
 menuOpen.addEventListener('click', () => {
-  console.log('メニューを開く');
+  // console.log('メニューを開く');
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+});
+
+// メニューを閉じる
+menuClose.addEventListener('click',() => {
+  menuPanel.animate({translate: [0, '100vw']},menuOptions);
 });
