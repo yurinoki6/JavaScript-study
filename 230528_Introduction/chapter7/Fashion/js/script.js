6eb0285a809afed5fd076cdc67bff3a93b6b448d
yurinoki6 <kuroehaneko@gmail.com>
2023-06-11 19:31:12 +0900

    アニメーションの指定をする

--- 230528_Introduction/chapter7/Fashion/js/script.js
+++ 230528_Introduction/chapter7/Fashion/js/script.js
@@ -120,7 +120,19 @@ menuClose.addEventListener('click', () => {
 const animateFade = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log(entry.target);
+      // console.log(entry.target);
+      entry.target.animate(
+        {
+          opacity: [0,1],
+          filter: ['blur(.4rem)','blur(0)'],
+          translate: ['0 4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
     }
   });
 };
